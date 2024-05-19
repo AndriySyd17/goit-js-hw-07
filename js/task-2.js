2c799a8fc70c71a92ed0f2a490a4ebbc64458d9d
AndriySyd17 <153460147+AndriySyd17@users.noreply.github.com>
2024-05-19 20:21:43 +0300

    7

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,11 @@
+const galleryList = document.querySelector('.gallery');
+
+const imagesMarkup = images.map(image => {
+    return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
+}).join('');
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
 const images = [
   {
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
